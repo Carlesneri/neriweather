97be6056187f5c867dd0e9199b0033e7dfce9df0
nx-carlosc <carlos.company@nexum.com>
2022-05-29 14:42:34 +0200

    feat: footer component

---
+++ src/components/Footer.svelte
@@ -0,0 +1,20 @@
+<script>
+  const year = new Date().getFullYear()
+</script>
+
+<footer>
+  neriweather {year} - no rights reserved at all
+</footer>
+
+<style>
+  footer {
+    color: #f45853;
+    text-align: center;
+    font-size: .6rem;
+    position: fixed;
+    margin: 0 auto;
+    bottom: 1rem;
+    left: 0;
+    width: 100%;
+  }
+</style>
