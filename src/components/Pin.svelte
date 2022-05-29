b5a4f95acc4eabb7c65a8742e588360ca17e26b3
nx-carlosc <carlos.company@nexum.com>
2022-05-29 14:43:32 +0200

    feat: component for pin locartion

---
+++ src/components/Pin.svelte
@@ -0,0 +1,18 @@
+<script lang='ts'>
+  export let pinned: Boolean;
+</script>
+
+<div>
+  <svg xmlns="http://www.w3.org/2000/svg" height="20px" viewBox="0 0 24 24" width="20px" fill={pinned ? '#f45853' : '#b4adb3'}><path d="M0 0h24v24H0z" fill="none"/><path d="M12 12c-1.1 0-2-.9-2-2s.9-2 2-2 2 .9 2 2-.9 2-2 2zm6-1.8C18 6.57 15.35 4 12 4s-6 2.57-6 6.2c0 2.34 1.95 5.44 6 9.14 4.05-3.7 6-6.8 6-9.14zM12 2c4.2 0 8 3.22 8 8.2 0 3.32-2.67 7.25-8 11.8-5.33-4.55-8-8.48-8-11.8C4 5.22 7.8 2 12 2z"/></svg>
+</div>
+
+<style>
+  svg {
+    cursor: pointer;
+    transition: filter 0.2s ease;
+  }
+
+  svg:hover {
+    filter: brightness(1.5);
+  }
+</style>
